2faa174c21f6f8777e6a6ba9eed8dfb0f0534d58
mmizukami <jplockongn006@gmail.com>
2016-09-06 17:06:11 -0700

    week2 9/6

---
+++ app/src/main/java/edu/orangecoastcollege/cs273/tapcounter/Counter.java
@@ -0,0 +1,15 @@
+package edu.orangecoastcollege.cs273.tapcounter;
+
+/**
+ * Created by mahiro mizukami on 9/6/2016.
+ */
+public class Counter
+{
+    private int mCount;
+
+    public Counter()
+    {mCount = 0;}
+    public void addCount()
+    {mCount++;}
+
+}
